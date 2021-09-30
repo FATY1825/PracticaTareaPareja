a1a8c38d823b821db2120ddc7a3450c29e695e61
Fatima Ayala <ftayala89@gmail.com>
2021-09-30 11:24:45 -0600

    Agregar archivos de proyecto.

---
+++ PracticaTareaPareja/Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaTareaPareja
+{
+    class Animal
+    {
+        //propiedades
+        public int animalAge { set; get; }
+
+
+    }
+}
